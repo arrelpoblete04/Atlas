edabeb2d95b557b81ff382d02f17b748b0e21a3a
Arrel Poblete <arrel.poblete@gmail.com>
2021-09-28 11:23:18 +0800

    Test unit test error

--- internal/Election/election_test.go
+++ internal/Election/election_test.go
@@ -34,7 +34,7 @@ func TestGetCandidateInfo(t *testing.T) {
 		urlPath    string
 		statusCode int
 	}{
-		{testCase: "Valid Name", name: "Arrel", urlPath: "/election2022", statusCode: http.StatusOK},
+		{testCase: "Valid Name", name: "Arrel", urlPath: "/election2022", statusCode: http.StatusBadRequest},
 		{testCase: "Blank Name", name: "", urlPath: "/election2022", statusCode: http.StatusBadRequest},
 		{testCase: "Error 404", name: "Arrel", urlPath: "/electio022", statusCode: http.StatusOK},
 	}
