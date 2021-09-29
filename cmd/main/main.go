c6552d3f624975a8d3714a51e94f44c7c8776e69
Arrel Poblete <arrel.poblete@gmail.com>
2021-09-29 17:03:04 +0800

    sample

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -33,7 +33,7 @@ func main() {
 
 	router.Use(appendRequestIdLogging())
 	router.GET("/election2022", election.ElectionController())
-	log.Info("ArgoCD Tekton Github")
+	log.Info("ArgoCD Tekton Github - PR")
 	log.Info("Serving readiness probe at port: 8000")
 	err := http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), router)
 
