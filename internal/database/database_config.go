e033100f2207b1296aae2e4d2b32223c11affc7f
Arrel Poblete <arrel.poblete@gmail.com>
2021-10-08 18:56:28 +0800

    Organized stuff

--- internal/database/database_config.go
+++ internal/database/database_config.go
@@ -1,49 +1,49 @@
 package database
 
-import (
-	"database/sql"
-	"os"
+// import (
+// 	"database/sql"
+// 	"os"
 
-	_ "github.com/go-sql-driver/mysql"
-	log "github.com/sirupsen/logrus"
-)
+// 	_ "github.com/go-sql-driver/mysql"
+// 	log "github.com/sirupsen/logrus"
+// )
 
-var (
-	db *sql.DB
-)
+// var (
+// 	db *sql.DB
+// )
 
-type Credentials struct {
-	User     string
-	Password string
-}
+// type Credentials struct {
+// 	User     string
+// 	Password string
+// }
 
-func Connect(credentials Credentials) {
+// func Connect(credentials Credentials) {
 
-	log.Infoln("Starting to connect to database")
-	log.Infof("MySQL Kube: %s", os.Getenv("MYSQL_KUBE"))
-	log.Infof("DB Username: %s", credentials.User)
-	log.Infof("DB Pass1word: %s", credentials.Password)
+// 	log.Infoln("Starting to connect to database")
+// 	log.Infof("MySQL Kube: %s", os.Getenv("MYSQL_KUBE"))
+// 	log.Infof("DB Username: %s", credentials.User)
+// 	log.Infof("DB Password: %s", credentials.Password)
 
-	d, err := sql.Open("mysql", os.Getenv("MYSQL_KUBE"))
+// 	d, err := sql.Open("mysql", os.Getenv("MYSQL_KUBE"))
 
-	if err != nil {
-		log.Fatalf("Error connecting to dasssstabase: %s", err.Error())
-	} else {
-		log.Infoln("Connected to database")
-	}
+// 	if err != nil {
+// 		log.Fatalf("Error connecting to database: %s", err.Error())
+// 	} else {
+// 		log.Infoln("Connected to database")
+// 	}
 
-	db = d
-}
+// 	db = d
+// }
 
-func DB(username, password string) *sql.DB {
+// func DB(username, password string) *sql.DB {
 
-	credentials := Credentials{
-		User:     username,
-		Password: password,
-	}
+// 	credentials := Credentials{
+// 		User:     username,
+// 		Password: password,
+// 	}
 
-	if db == nil {
-		Connect(credentials)
-	}
-	return db
-}
+// 	if db == nil {
+// 		Connect(credentials)
+// 	}
+// 	return db
+// }
